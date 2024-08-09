95abe1aac69a491557914ac58dfe50f52e9f0cbc
ProggerX <x@proggers.ru>
2024-08-09 15:31:15 +0300

    Update

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/NotBalds/cwe_server
 
 go 1.22.3
 
-require github.com/danielgtaylor/huma/v2 v2.18.0
+require (
+	github.com/danielgtaylor/huma/v2 v2.18.0
+	github.com/joho/godotenv v1.5.1
+)
--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/danielgtaylor/huma/v2"
 	"github.com/danielgtaylor/huma/v2/adapters/humago"
+	"github.com/joho/godotenv"
 )
 
 func exists(path string) bool {
@@ -18,6 +19,9 @@ func exists(path string) bool {
 }
 
 func main() {
+	godotenv.Load()
+	os.Mkdir("v0.3.0", os.ModePerm)
+	os.Chdir("v0.3.0")
 	if !exists("db.json") {
 		os.WriteFile("db.json", []byte("{}"), fs.ModePerm)
 	}
@@ -47,5 +51,5 @@ func main() {
 		Summary:      "Send a message",
 		MaxBodyBytes: 20 * 1024 * 1024}, sendMessage)
 
-	http.ListenAndServe(":1337", mux)
+	http.ListenAndServe(":"+os.Getenv("CWE_PORT"), mux)
 }
